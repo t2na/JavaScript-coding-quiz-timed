402e6e517d20e664a6ecafa50a1ae8ae066861d6
Zach Antunes <ztunes4@gmail.com>
2023-05-26 16:10:51 -0700

    Finished questions 1-4, going to pause now to add styling

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,5 @@
 var startButton = document.querySelector(".starting-page");
 var startBtnEl = document.querySelector("#startbtn");
-var questionOnePage = document.querySelector(".question-1-page");
-var questionOneAnswers = document.querySelectorAll(".answer-1-button");
-var questionTwoPage = document.querySelector(".question-2-page");
 
 startBtnEl.addEventListener("click", function (event) {
     console.log("Button Clicked");
@@ -10,28 +7,31 @@ startBtnEl.addEventListener("click", function (event) {
     questionOnePage.classList.remove("hidden");
 })
 
-var correctAnswerOne = document.getElementById("1-choice-A");
-var AnswerOneCorrect = document.querySelector(".Answer-One-Correct");
-var AnswerOneIncorrect = document.querySelector(".Answer-One-Incorrect");
+var questionOnePage = document.querySelector(".question-1-page");
+var questionOneAnswers = document.querySelectorAll(".answer-1-button");
+var questionTwoPage = document.querySelector(".question-2-page");
+
+var correctAnswerOne = document.getElementById("choice-1-A");
+var questionOneCorrect = document.querySelector(".Question-One-Correct");
+var questionOneIncorrect = document.querySelector(".Question-One-Incorrect");
 var questionOneNext = document.querySelector(".question-1-page");
 
 questionOneAnswers.forEach(function(button) {
     button.addEventListener('click', function(event) {
         // if answer A clicked = true
-        if (event.target.id === "1-choice-A") {
-            console.log("Answer One Correct!");
-            AnswerOneCorrect.classList.remove("hidden");
-        // need to disable the other buttons
-        document.querySelectorAll('.answer-1-button:not(#1-choice-A)').forEach(function(button) {
-            button.disabled = true;
-        });
-        
-
+        if (event.target.id === "choice-1-A") {
+            console.log("Question One Correct!");
+            questionOneCorrect.classList.remove("hidden");
+        // need to disable the other buttons 
         } else {
-            console.log("Answer One Incorrect!")
-            AnswerOneIncorrect.classList.remove("hidden");
+            console.log("Question One Incorrect!")
+            questionOneIncorrect.classList.remove("hidden");
         }
         // How do I make it so that once one selection is made, no other selections can be made i.e. if someone clicks the correct answer, if they were to click another button, 
+        document.querySelectorAll('.answer-1-button').forEach(function(button) {
+            console.log("Disabling button", button.id);
+            button.disabled = true;
+        });
         setTimeout(() => {
         questionOneNext.classList.add("hidden");
         questionTwoPage.classList.remove("hidden");
@@ -39,7 +39,69 @@ questionOneAnswers.forEach(function(button) {
     });
 });
 
+var questionTwoPage = document.querySelector(".question-2-page");
+var questionTwoAnswers = document.querySelectorAll(".answer-2-button");
+var questionThreePage = document.querySelector(".question-3-page");
 
+var correctAnswerTwo = document.getElementById("choice-2-A");
+var questionTwoCorrect = document.querySelector(".Question-Two-Correct");
+var questionTwoIncorrect = document.querySelector(".Question-Two-Incorrect");
+var questionTwoNext = document.querySelector(".question-2-page");
+
+questionTwoAnswers.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        // if answer A clicked = true
+        if (event.target.id === "choice-2-B") {
+            console.log("Question Two Correct!");
+            questionTwoCorrect.classList.remove("hidden");
+        // need to disable the other buttons 
+        } else {
+            console.log("Question Two Incorrect!")
+            questionTwoIncorrect.classList.remove("hidden");
+        }
+        // How do I make it so that once one selection is made, no other selections can be made i.e. if someone clicks the correct answer, if they were to click another button, 
+        document.querySelectorAll('.answer-2-button').forEach(function(button) {
+            console.log("Disabling button", button.id);
+            button.disabled = true;
+        });
+        setTimeout(() => {
+        questionTwoNext.classList.add("hidden");
+        questionThreePage.classList.remove("hidden");
+        }, 2000);
+    });
+});
+
+var questionThreePage = document.querySelector(".question-3-page");
+var questionThreeAnswers = document.querySelectorAll(".answer-3-button");
+var questionFourPage = document.querySelector(".question-4-page");
+
+var correctAnswerThree = document.getElementById("choice-3-A");
+var questionThreeCorrect = document.querySelector(".Question-Three-Correct");
+var questionThreeIncorrect = document.querySelector(".Question-Three-Incorrect");
+var questionThreeNext = document.querySelector(".question-3-page");
+
+questionThreeAnswers.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        
+        if (event.target.id === "choice-3-A") {
+            console.log("Question Three Correct!");
+            questionThreeCorrect.classList.remove("hidden");
+      
+        } else {
+            console.log("Question Three Incorrect!")
+            questionThreeIncorrect.classList.remove("hidden");
+        }
+
+        document.querySelectorAll('.answer-3-button').forEach(function(button) {
+            console.log("Disabling button", button.id);
+            button.disabled = true;
+        });
+        setTimeout(() => {
+        questionThreeNext.classList.add("hidden");
+        questionFourPage.classList.remove("hidden");
+        }, 2000);
+    });
+});
 
 
 
